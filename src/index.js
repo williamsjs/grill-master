50a3bdfe5b81b812b76d0c79d62727be86a02d32
williamsjs <williamsjs1224@gmail.com>
2018-04-24 19:26:54 -0400

    import main stylesheet to app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
+import styles from './styles/main.scss';
 import App from './components/App/App';
 
 render(<App />, document.getElementById('root'))
