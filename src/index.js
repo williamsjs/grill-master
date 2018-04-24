702d9725da06640da2f5a7893402be38bdd1135e
williamsjs <williamsjs1224@gmail.com>
2018-04-24 19:09:47 -0400

    add babel preset react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
 import style from './main.css';
+
+import App from './components/App/App';
+
+render(<App />, document.getElementById('root'))
