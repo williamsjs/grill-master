f5e04029afec4c2d57de23d9c9b6efd857e75c53
williamsjs <williamsjs1224@gmail.com>
2018-04-26 20:51:14 -0400

    add styles

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="main-nav">
       <li><a href="#">Home</a></li>
       <li><a href="#">Recipes</a></li>
       <li><a href="#">Calendar</a></li>
