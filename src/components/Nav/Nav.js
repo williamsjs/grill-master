553bc714dd0de694ce52e373a0ef9f2d3efb7c04
williamsjs <williamsjs1224@gmail.com>
2018-04-27 21:51:42 -0400

    add hideous search button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,7 @@ import IoBeer from 'react-icons/lib/io/beer';
 import IoFork from 'react-icons/lib/io/fork';
 import IoIosContact from 'react-icons/lib/io/ios-contact';
 import IoBonfire from 'react-icons/lib/io/bonfire';
+import IoIosSearchStrong from 'react-icons/lib/io/ios-search-strong';
 
 const Nav = () => {
   return (
@@ -10,6 +11,7 @@ const Nav = () => {
       <h1 className="title nav-item">Beer Meat</h1>
       <form class="nav-item">
         <input type="search" placeholder="search"/>
+        <button type="submit"><IoIosSearchStrong /></button>
       </form>
       <li className="nav-item"><a href="#"><IoBonfire />What's Hot</a></li>
       <li className="nav-item"><a href="#"><IoFork />Meat</a></li>
