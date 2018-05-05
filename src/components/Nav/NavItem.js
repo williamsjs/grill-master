525844e88fd65a3f502f4a1937dc8fa40965801d
williamsjs <williamsjs1224@gmail.com>
2018-05-04 22:32:32 -0400

    close menu when link is pressed

--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavItem = ({link, linkText, icon}) => {
+const NavItem = ({link, linkText, icon, handleClick}) => {
   return (
     <li className="nav-item">
-      <NavLink to={link} activeClassName="link-active">{icon} &nbsp;{linkText}</NavLink>
+      <NavLink to={link} activeClassName="link-active" onClick={handleClick}>{icon} &nbsp;{linkText}</NavLink>
     </li>
   );
 };
