7445544b8e6ef67d7f85f3facbec63e2bfbc27b2
williamsjs <williamsjs1224@gmail.com>
2018-05-06 18:15:36 -0400

    add redux thunk

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
 
 export default store;
