ec212ac24f8efd47a73247d1340ab220dd776876
williamsjs <williamsjs1224@gmail.com>
2018-05-08 08:03:45 -0400

    add classes to card, cardlist

--- src/components/Shared/CardList/CardList.js
+++ src/components/Shared/CardList/CardList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './CardList.scss';
 
 const CardList = ({items}) => (
   <div className="card-list">
--- src/components/shared/Card/Card.js
+++ src/components/shared/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.scss';
 
 const Card = ({name}) => (
-  <div style={{width: '25%', display: 'inline-block'}}>
+  <div className="card">
     <img className="card-image" src="https://picsum.photos/200/300" alt="no image display" />
     <h1 className="card-title">{name}</h1>
     {name}
