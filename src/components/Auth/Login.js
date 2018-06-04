c3a78cb2d8c72098e77cabf5bad930cd36b41eec
williamsjs <williamsjs1224@gmail.com>
2018-06-04 17:59:15 -0400

    restyle left nav

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,16 +3,19 @@ import ReactLoading from 'react-loading';
 import IoLogIn from 'react-icons/lib/io/log-in';
 
 const Login = ({handleSubmit, handleChange, email, password, fetching, failure}) => {
+
+  const borderRadius = () => ({borderRadius: '25px'});
+
   return (
     <div className="login-form">
       <form onSubmit={handleSubmit} onClick={e => e.stopPropagation()} className="form-container center" style={{borderRadius: '0 0 5px 5px'}}>
         <div className="form-group center">
           <label className="form-label">Email: </label>
-          <input onChange={handleChange} value={email} className="form-control" type="text" placeholder="email" />
+          <input onChange={handleChange} value={email} style={borderRadius()} className="form-control" type="text" placeholder="email" />
         </div>
         <div className="form-group">
           <label className="form-label">Password: </label>
-          <input onChange={handleChange} value={password} className="form-control" type="password" placeholder="password" />
+          <input onChange={handleChange} value={password} style={borderRadius()} className="form-control" type="password" placeholder="password" />
         </div>
 
         {fetching &&
